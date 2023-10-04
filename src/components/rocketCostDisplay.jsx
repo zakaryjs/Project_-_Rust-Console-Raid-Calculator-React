dabf7224073251246881ae1fd393fc87640f391a
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-10-04 15:31:35 +0800

    feat: updated image and div css

--- src/components/rocketCostDisplay.jsx
+++ src/components/rocketCostDisplay.jsx
@@ -5,15 +5,17 @@ export default function RocketCostDisplay( { rocketValue }) {
     if (rocketValue) {
         return (
             <div>
-            <p>Sulphur: {rocketValue.rocketSulphur}</p>
-            <p>Gunpowder: {rocketValue.rocketGunpowder}</p>
-            <p>Pipes: {rocketValue.rocketPipes}</p>
-            <p>Explosives: {rocketValue.rocketExplosives}</p>
-            <img width={120} height={120} src={require('../images/explosives.png')} alt="explosives icon"/>
-            <p>Explosives Sulphur: {rocketValue.explosivesSulphur}</p>
-            <p>Explosives Gunpowder: {rocketValue.explosivesGunpowder}</p>
-            <p>Explosives Low Grade: {rocketValue.explosivesLowGrade}</p>
-            <p>Explosives Frags: {rocketValue.explosivesFrags}</p>
+                <div>
+                    <p>Sulphur: {rocketValue.rocketSulphur}</p>
+                    <p>Gunpowder: {rocketValue.rocketGunpowder}</p>
+                    <p>Pipes: {rocketValue.rocketPipes}</p>
+                    <p>Explosives: {rocketValue.rocketExplosives}</p>
+                    <img width={65} height={65} src={require('../images/explosives.png')} alt="explosives icon"/>
+                    <p>Explosives Sulphur: {rocketValue.explosivesSulphur}</p>
+                    <p>Explosives Gunpowder: {rocketValue.explosivesGunpowder}</p>
+                    <p>Explosives Low Grade: {rocketValue.explosivesLowGrade}</p>
+                    <p>Explosives Frags: {rocketValue.explosivesFrags}</p>
+                </div>
             </div>  
             )
     }
